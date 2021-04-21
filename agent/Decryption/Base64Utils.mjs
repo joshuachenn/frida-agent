02dba7cb83a1639e673fdfe004b77a791e2c5db9
joshuachenn <joshuaczh@163.com>
2021-04-20 21:06:34 -0700

    ncsoft hooking update

---
+++ agent/Decryption/Base64Utils.mjs
@@ -0,0 +1,5 @@
+
+
+function somefunc() {
+    console.log('some func')
+}
--- agent/lineagemhook.js
+++ agent/lineagemhook.js
@@ -1,8 +1,224 @@
+(function(){
+    let Color = {RESET: "\x1b[39;49;00m", Black: "0;01", Blue: "4;01", Cyan: "6;01", Gray: "7;11", "Green": "2;01", Purple: "5;01", Red: "1;01", Yellow: "3;01"};
+    let LightColor = {RESET: "\x1b[39;49;00m", Black: "0;11", Blue: "4;11", Cyan: "6;11", Gray: "7;01", "Green": "2;11", Purple: "5;11", Red: "1;11", Yellow: "3;11"};    
+    var colorPrefix = '\x1b[3', colorSuffix = 'm'
+    for (let c in Color){
+        if (c  == "RESET") continue;
+        console[c] = function(message){
+            console.log(colorPrefix + Color[c] + colorSuffix + message + Color.RESET);
+        }
+        console["Light" + c] = function(message){
+            console.log(colorPrefix + LightColor[c] + colorSuffix + message + Color.RESET);
+        }
+    }
+})();
+function uniqBy(array, key) {
+    var seen = {};
+    return array.filter(function (item) {
+        var k = key(item);
+        return seen.hasOwnProperty(k) ? false : (seen[k] = true);
+    });
+}
+function hasOwnProperty(obj, name) {
+    try {
+        return obj.hasOwnProperty(name) || name in obj;
+    } catch (e) {
+        return obj.hasOwnProperty(name);
+    }
+}
+function getHandle(object) {
+    if (hasOwnProperty(object, '$handle')) {
+        if (object.$handle != undefined) {
+            return object.$handle;
+        }
+    }
+    if (hasOwnProperty(object, '$h')) {
+        if (object.$h != undefined) {
+            return object.$h;
+        }
+    }
+    return null;
+}
+//查看域值
+function inspectObject(obj, input) {
+    var isInstance = false;
+    var obj_class = null;
+    if (getHandle(obj) === null) {
+        obj_class = obj.class;
+    } else {
+        var Class = Java.use("java.lang.Class");
+        obj_class = Java.cast(obj.getClass(), Class);
+        isInstance = true;
+    }
+    input = input.concat("Inspecting Fields: => ", isInstance, " => ", obj_class.toString());
+    input = input.concat("\r\n")
+    var fields = obj_class.getDeclaredFields();
+    for (var i in fields) {
+        if (isInstance || Boolean(fields[i].toString().indexOf("static ") >= 0)) {
+            // output = output.concat("\t\t static static static " + fields[i].toString());
+            var className = obj_class.toString().trim().split(" ")[1];
+            // console.Red("className is => ",className);
+            var fieldName = fields[i].toString().split(className.concat(".")).pop();
+            var fieldType = fields[i].toString().split(" ").slice(-2)[0];
+            var fieldValue = undefined;
+            if (!(obj[fieldName] === undefined))
+                fieldValue = obj[fieldName].value;
+            input = input.concat(fieldType + " \t" + fieldName + " => ", fieldValue + " => ", JSON.stringify(fieldValue));
+            input = input.concat("\r\n")
+        }
+    }
+    return input;
+}
+
+// trace单个类的所有静态和实例方法包括构造方法 trace a specific Java Method
+function traceMethod(targetClassMethod) {
+    var delim = targetClassMethod.lastIndexOf(".");
+    if (delim === -1) return;
+    var targetClass = targetClassMethod.slice(0, delim)
+    var targetMethod = targetClassMethod.slice(delim + 1, targetClassMethod.length)
+    var hook = Java.use(targetClass);
+    var overloadCount = hook[targetMethod].overloads.length;
+    console.Red("Tracing Method : " + targetClassMethod + " [" + overloadCount + " overload(s)]");
+    for (var i = 0; i < overloadCount; i++) {
+        hook[targetMethod].overloads[i].implementation = function () {
+            //初始化输出
+            var output = "";
+            //画个横线
+            for (var p = 0; p < 100; p++) {
+                output = output.concat("==");
+            }
+            console.Gray(output);
+            var output = "";
+            //域值
+            output = inspectObject(this, output);
+            console.Blue(output);
+            var output = "";
+            //进入函数
+            output = output.concat("\n*** entered " + targetClassMethod);
+            output = output.concat("\r\n")
+            if (arguments.length) console.Black();
+            //参数
+            for (var j = 0; j < arguments.length; j++) {
+                output = output.concat("arg[" + j + "]: " + arguments[j] + " => " + JSON.stringify(arguments[j]));
+                output = output.concat("\r\n")
+            }
+            //调用栈
+            output = output.concat(Java.use("android.util.Log").getStackTraceString(Java.use("java.lang.Throwable").$new()));
+            console.Green(output);
+            var output = "";
+            var retval = this[targetMethod].apply(this, arguments);
+            //返回值
+            output = output.concat("\nretval: " + retval + " => " + JSON.stringify(retval));
+            // inspectObject(this)
+            //离开函数
+            output = output.concat("\n*** exiting " + targetClassMethod);
+            //最终输出
+            console.Black(output);
+            return retval;
+        }
+    }
+}
+
+function traceClass(targetClass) {
+
+    //Java.use是新建一个对象哈，大家还记得么？
+    var hook = Java.use(targetClass);
+    //利用反射的方式，拿到当前类的所有方法
+    var methods = hook.class.getDeclaredMethods();    
+    //建完对象之后记得将对象释放掉哈
+    hook.$dispose;
+    //将方法名保存到数组中
+    var parsedMethods = [];
+    var output = "";    
+    output = output.concat("\tSpec: => \r\n")
+    methods.forEach(function (method) {
+        output = output.concat(method.toString())
+        output = output.concat("\r\n")
+        parsedMethods.push(method.toString().replace(targetClass + ".", "TOKEN").match(/\sTOKEN(.*)\(/)[1]);
+    });
+    //去掉一些重复的值
+    var Targets = uniqBy(parsedMethods, JSON.stringify);
+    // targets = [];
+    var constructors = hook.class.getDeclaredConstructors();
+    if (constructors.length > 0) {
+        constructors.forEach(function (constructor) {
+            output = output.concat("Tracing ", constructor.toString())
+            output = output.concat("\r\n")
+        })
+        Targets = Targets.concat("$init")
+    }
+    //对数组中所有的方法进行hook，
+    Targets.forEach(function (targetMethod) {
+        traceMethod(targetClass + "." + targetMethod);
+    });
+    //画个横线
+    for (var p = 0; p < 100; p++) {
+        output = output.concat("+");
+    }
+    console.Green(output);
+}
+function hook(white, black, target = null) {
+    console.Red("start")
+    if (!(target === null)) {
+        console.LightGreen("Begin enumerateClassLoaders ...")
+        Java.enumerateClassLoaders({
+            onMatch: function (loader) {
+                try {
+                    if (loader.findClass(target)) {
+                        console.Red("Successfully found loader")
+                        console.Blue(loader);
+                        Java.classFactory.loader = loader;
+                        console.Red("Switch Classloader Successfully ! ")
+                    }
+                }
+                catch (error) {
+                    console.Red(" continuing :" + error)
+                }
+            },
+            onComplete: function () {
+                console.Red("EnumerateClassloader END")
+            }
+        })
+    }
+    console.Red("Begin Search Class...")
+    var targetClasses = new Array();
+    Java.enumerateLoadedClasses({
+        onMatch: function (className) {
+            if (className.toString().indexOf(white) >= 0 &&
+                className.toString().indexOf(black) < 0
+            ) {
+                console.Black("Found Class => ", className)
+                targetClasses.push(className);
+                traceClass(className);
+            }
+        }, onComplete: function () {
+            console.Black("Search Class Completed!")
+        }
+    })
+    var output = "On Total Tracing :"+String(targetClasses.length)+" classes :\r\n";
+    targetClasses.forEach(function(target){
+        output = output.concat(target);
+        output = output.concat("\r\n")        
+    })
+    console.Green(output+"Start Tracing ...")
+}
+
+
+
+
+
+
+
+
+
+
+
 /*
 
 use frida_dump to dump target so
 
 */
+
 
 function LogPrint(log) {
     var theDate = new Date();
@@ -44,6 +260,30 @@ function printJavaStackTrace(name) {
             Exception.$dispose()
         }
     })
+}
+
+function byteToString(arr) {
+    if (typeof arr === 'string') {
+        return arr;
+    }
+    var str = '',
+        _arr = arr;
+    for (var i = 0; i < _arr.length; i++) {
+        var one = _arr[i].toString(2),
+            v = one.match(/^1+?(?=0)/);
+        if (v && one.length == 8) {
+            var bytesLength = v[0].length;
+            var store = _arr[i].toString(2).slice(7 - bytesLength);
+            for (var st = 1; st < bytesLength; st++) {
+                store += _arr[st + i].toString(2).slice(2);
+            }
+            str += String.fromCharCode(parseInt(store, 2));
+            i += bytesLength - 1;
+        } else {
+            str += String.fromCharCode(_arr[i]);
+        }
+    }
+    return str;
 }
 
 var lineagem_fp = null
@@ -379,60 +619,89 @@ function native_dumphex() {
     // console.log(targetptr.readByteArray(0x100))
 }
 
+function Java_LoadS() {
+    if (Java.available) {
+        Java.perform(function() {
+            var NCGPApplication = Java.use('com.ncsoft.ncgp.NCGPApplication')
+            NCGPApplication.LoadS.implementation = function(arg0) {
+                var retVal = this.LoadS(arg0)
+                console.log("LoadS:"+arg0.toString() +" key:"+retVal.toString())
+                return retVal
+            }
+
+            // var hashArray = ['07201ee4c65e4213b5afa86a92a1ef05',
+            //                 '391a08fdae9542d1a128b58427807ffe',
+            //                 '7c6e8bbddd3f45719d9e7743e78de879',
+            //                 '302f03dc9eaf4317a3e81d8266b7bd8c',
+            //                 'a343915fd43a4dbab3fd52eb726d64d8',
+            //                 '82545c6ca4784f1589d9dd2e59bc89a2',
+            //                 'fcb190fa662943c0a7a6cf7bac4c4cc8',
+            //                 '878c4f749b4e4f249559cd81fb95fa6a',
+            //                 'b33a304427414e9f9dac09a6051cf0ac',
+            //                 'd282d8d40359480a9fb6cb77c130d8e1',
+            //                 '610951d7b4f84c0e99b16431d16422c1',
+            //                 '3b5df2700100489896e6609c5d7e12ee']
+            // for (var i = 0; i < hashArray.length; i++) {
+            //     var hashKey = hashArray[i];
+            //     var key = NCGPApplication.LoadS(hashKey)
+            //     console.log(hashKey+": "+key+";")
+            // }
+            // var hashKey = ''
+            // var key = NCGPApplication.LoadS(hashKey)
+            // console.log(hashKey+": "+key+";")
+        })
+    }
+}
+
+
+function decodeJwtHeader() {
+    if (Java.available) {
+        Java.perform(function() {
+            var Base64 = Java.use('android.util.Base64')
+            Java.enumerateClassLoaders({
+                onMatch:function(loader){
+                    try {
+                        if(loader.findClass("com.ncsoft.android.mop.apigate.HttpHelper")){
+                            console.log("Succefully found loader!",loader);
+                            Java.classFactory.loader = loader;                            
+
+                            Java.use('com.ncsoft.android.mop.apigate.HttpHelper').buildJwtHeader.implementation = function(arg0, arg1, arg2, arg3) {
+                                var headerString = this.buildJwtHeader(arg0, arg1, arg2, arg3)
+                                var base64EncodeAry = headerString.split('.')
+                                // console.log("now logging base64 encode params:", arg2, arg3)
+                                // JWT base64encodexxxx
+                                console.log("JWT "+ byteToString(Base64.decode(base64EncodeAry[0].split(' ')[1], 8)))
+                                console.log(byteToString(Base64.decode(base64EncodeAry[1], 8)))
+                                // part 3 not decrypted, current not concern
+                                return headerString
+                            }
+                        }
+                    } catch (error) {
+                        // console.log("found error "+error)
+                        
+                    }
+                },onComplete:function(){"enum completed!"}
+            })
+
+
+        })
+    }
+}
+
 function hook_ncgp() {
 
-    Java.perform(function() {
-        var NCGPApplication = Java.use('com.ncsoft.ncgp.NCGPApplication')
-        NCGPApplication.LoadS.implementation = function(arg0) {
-            var retVal = this.LoadS(arg0)
-            console.log('>==================LoadS===================<')
-            console.log(arg0.toString())
-            // printJavaStackTrace('NCGPApplication.LoadS')
-            console.log("ret:", retVal.toString())
-            console.log('^==================LoadS end===================^')
-            return retVal
-        }
-        var key = NCGPApplication.LoadS('878c4f749b4e4f249559cd81fb95fa6a')
-        console.log('++++++++key:', key)
-    })
-
     var libncgp = Process.findModuleByName('libncgp.so')
-    var loadS_addr = (libncgp.base).add(0xD0778)
-    console.log("loads_addr:", loadS_addr)
 
-    Interceptor.attach(ptr(loadS_addr), {
+    Interceptor.attach(ptr((libncgp.base).add(0xD0778)), {
         onEnter: function(args) {
-            
-            console.log("on enter Java_com_ncsoft_ncgp_NCGPApplication_LoadS")
-            // console.log("InParam:", args[1], hexdump(ptr(this.context.x1)))
-            // printNativeStack(this.context, 'LoadS')
+        
         }, onLeave: function(retval) {
             var env = Java.vm.getEnv()
             console.log('OutParam:', env.getStringUtfChars(retval, null).readCString())
         }
     })
-
-    var subB29DC = libncgp.base.add(0xB29DC)
-
-    Interceptor.attach(subB29DC, {
-        onEnter: function(args) {
-            console.log("+++onEnter subB29DC: ", args[1].readCString())
-            console.log("arg1:", args[1])
-            printNativeStack(this.context, 'subB29DC')
-        }, onLeave:function(retval) {
-        }
-    })
-
-    var T3hUpz39vsuWrmlDyEe9 = Module.findExportByName('libncgp.so', 'T3hUpz39vsuWrmlDyEe9')
-    Interceptor.attach(T3hUpz39vsuWrmlDyEe9, {
-        onEnter:function(args) {
-            console.log("============= enter: T3hUpz39vsuWrmlDyEe9")
-        }, onLeave: function(retval) {
-
-        }
-    })
-
 }
+
 
 function hook_strstr() {
     // var libcmodule = Process.getModuleByName("libc.so");
@@ -452,26 +721,74 @@ function hook_strstr() {
       },
       onLeave: function(retval) {
 
-        if(this.flag.indexOf("RegisterNativeFlag") != -1) {
-            console.log("RegisterNativeFlag:"+this.info);
-        }
+        // if(this.flag.indexOf("RegisterNativeFlag") != -1) {
+        //     console.log("RegisterNativeFlag:"+this.info);
+        // }
 
         if (this.flag.indexOf("JniMethodStart") != -1) {
           // if (this.info.indexOf("MainActivity.onCreate") != -1) {
           //   console.log("MainActivity.onCreate:"+this.info);
           //   retval.replace(0x1234);
           // }
-          if (this.info.indexOf("NCGPApplication.LoadS") != -1) {
-            // if (++times > 1) {
-              console.log("NCGPApplication.LoadS:"+this.info);
-            //   retval.replace(0x1234);
-            // }
-          }
+        //   if (this.info.indexOf("NCGPApplication.LoadS") != -1) {
+        //     // if (++times > 1) {
+        //       console.log("NCGPApplication.LoadS:"+this.info);
+        //     //   retval.replace(0x1234);
+        //     // }
+        //   }
+
+            // filter 
+            // org.cocos2dx.lib.Cocos2dxRenderer.nativeRender
+            // eglSwapBuffers
+            // nanoTime
+            if (this.info.indexOf('nativeReadString') != -1 ||
+            this.info.indexOf('Cocos2dxRenderer.nativeRender') != -1 ||
+            this.info.indexOf('eglSwapBuffers') != -1 ||
+            this.info.indexOf('nanoTime') != -1 ) {
+                return
+            }
+            // void android.view.InputEventReceiver.nativeFinishInputEvent(long, int, boolean)
+            if (this.info.indexOf('nativeTouchesBegin') != -1) {
+                console.log("JnimethodStart:", this.info)
+            }
         }
 
       }
     })
   }
+
+function hook_login() {
+    // var touchesBegin = Module.findExportByName('nativeTouchesBegin')
+    if (Java.available) {
+        Java.perform(function() {
+            Java.enumerateClassLoaders({
+                onMatch:function(loader){
+                    try {
+                        if(loader.findClass("com.ncsoft.android.mop.AuthManager")){
+                            var AuthManager = Java.use('com.ncsoft.android.mop.AuthManager')
+                            AuthManager.loginViaPlaync.implementation = function(arg0, arg1, arg2, arg3, arg4) {
+                                console.log("loginViaPlaync")
+                                return this.loginViaPlaync(arg0, arg1, arg2, arg3, arg4)
+                            }
+                            AuthManager.loginViaGuest.implementation = function(arg0, arg1, arg2) {
+                                console.log("loginViaGuest")
+                                return this.loginViaGuest(arg0, arg1, arg2)
+                            }
+                            AuthManager.verifySession.implementation = function(arg0, arg1) {
+                                console.log('verifySession called')
+                                printJavaStackTrace('verifySession')
+                                return this.verifySession(arg0, arg1)
+                            }
+                        }
+                    } catch (error) {
+
+                    }
+                }, onComplete: function() {}
+            })
+
+        })
+    }
+}
 
 function hook_bypassAntiDebug() {
     var ptrace_addr = Module.findExportByName(null, "ptrace")
@@ -504,8 +821,17 @@ function hook_bypassAntiDebug() {
 function main() {
     hook_bypassAntiDebug()
     // hook_doApiRequest()
-    hook_ncgp()
+    // hook_ncgp()
     // hook_strstr()
+    // Java_LoadS()
+    // decodeJwtHeader()
+
+    // hook_login()
+    Java.perform(function() {
+        // traceClass("com.ncsoft.android.mop.AuthManager")
+        // traceClass('com.ncsoft.android.mop.apigate.requests.AuthRequest')
+        traceClass('com.ncsoft.android.mop.NcHttpRequest')
+    })
 
 }
 
